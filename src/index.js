c1f2d4d7f176575aacbad1746d59241772501e69
Maaz4f <114129442+Maaz4f@users.noreply.github.com>
2023-03-05 06:43:32 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import resta from './assets/resta.png';
+import user from './assets/user.png';
+import './Show.css'
+import { ReactComponent as MoonIcon } from './assets/MoonIcon.svg';
+import { ReactComponent as SunIcon } from './assets/SunIcon.svg';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
